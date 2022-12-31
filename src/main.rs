c35b2cd22942af165f9c55ea8f896145842b7dcf
Matt London <mattlondon57@gmail.com>
2022-12-30 21:53:09 -0500

    Add simple contains test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod constants;
+
+use crate::constants::*;
+
 fn main() {
     println!("Hello, world!");
+
+    if FUNCTION_OPERATION.contains(Operation::Function) {
+        println!("Function in function op");
+    }
+
+    if !FUNCTION_OPERATION.contains(Operation::Add) {
+        println!("Add in function not in op");
+    }
 }
