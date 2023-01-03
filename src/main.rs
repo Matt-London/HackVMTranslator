ddf3bb33dcb10ecf5c7d3636c7ad91d147073a4b
Matt London <mattlondon57@gmail.com>
2023-01-03 02:06:04 -0500

    Implement clap to make a help message

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,28 @@ mod operations;
 mod parser;
 
 use crate::operations::*;
-use crate::parser::Parser;
 
-use std::env;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    /// Input file path
+    input_path: String,
+    /// Output file path
+    output_path: Option<String>,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Cli::parse();
 
-    let mut parser = Parser::new(&args[1]);
+    let mut parser = parser::Parser::new(&args.input_path);
 
-    parser.output(&args[2]);
+    // Get the default name (input_path with .asm)
+    let default_name = args.input_path.replace(".vm", ".asm");
+
+    match args.output_path {
+        Some(output) => parser.output(&output),
+        None => parser.output(&default_name)
+    }
     
 }
