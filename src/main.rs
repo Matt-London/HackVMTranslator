dc46198657fd6dfb1d9ccba45c5ae6dc816a1d50
Matt London <mattlondon57@gmail.com>
2023-01-09 23:02:47 -0500

    Add support for directories

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Cli {
-    /// Input file path
+    /// Input file/directory path
     input_path: String,
     /// Output file path
     output_path: Option<String>,
@@ -20,7 +20,28 @@ fn main() {
     let mut parser = parser::Parser::new(&args.input_path);
 
     // Get the default name (input_path with .asm)
-    let default_name = args.input_path.replace(".vm", ".asm");
+
+    let default_name;
+    
+    if args.input_path.contains(".vm") {
+        default_name = args.input_path.replace(".vm", ".asm");
+    }
+    else {
+        let unix_path = args.input_path.replace("\\", "/").to_owned();
+
+        let name_stripped = unix_path.split("/").collect::<Vec<&str>>();
+
+        let mut base_name = "";
+
+        for word in name_stripped.iter().rev() {
+            if word != &"" {
+                base_name = word;
+                break;
+            }
+        }
+
+        default_name = format!("{}/{}.asm", args.input_path, base_name);
+    }
 
     match args.output_path {
         Some(output) => parser.output(&output),
--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,13 +1,22 @@
 use crate::parser::Command;
 
-use std::fs::File;
+use std::fs::{File, metadata, read_dir};
 use std::path::Path;
 use std::io::{prelude::*, BufReader};
 
 use substring::Substring;
 
+#[derive(PartialEq, Eq)]
+enum FileType {
+    None,
+    File,
+    Directory
+}
+
 pub struct Parser {
     vm_path: String,
+    file_type: FileType,
+    cmd_cnt: u32,
     command_list: Vec<Command>
 }
 
@@ -15,32 +24,82 @@ impl Parser {
     pub fn new(fname: &str) -> Self {
         let mut parser = Parser {
             vm_path: fname.to_owned(),
+            file_type: FileType::None,
+            cmd_cnt: 0,
             command_list: Vec::new()
         };
 
-        parser.process_cmds();
+        parser.process_project();
 
         return parser;
     }
 
-    fn process_cmds(&mut self) {
+    /// Writes the initializer code into the output list
+    fn write_init(&mut self) {
+        let mut command = Command::new("", self.cmd_cnt, "");
+        self.cmd_cnt += 1;
+        command.write_init();
+        self.command_list.push(command);
+
+    }
+
+    /// Processes a vm file and appends its commands to the command_list
+    fn process_file(&mut self, file_path: &str) {
         // Get base file name
-        let path = Path::new(&self.vm_path);
+        let path = Path::new(file_path);
         let full_name = path.file_name().unwrap().to_os_string().into_string().unwrap();
 
         let file_name = full_name.substring(0, full_name.find(".").unwrap());
 
-        let vm_code = File::open(&self.vm_path).unwrap();
+        let vm_code = File::open(file_path).unwrap();
         let bf = BufReader::new(vm_code);
 
-        let mut cmd_cnt = 0;
         for line  in bf.lines() {
-            let command = Command::new(&line.unwrap(), cmd_cnt, file_name);
+            let command = Command::new(&line.unwrap(), self.cmd_cnt, file_name);
 
             if command.has_command() {
                 self.command_list.push(command);
-                cmd_cnt += 1;
+                self.cmd_cnt += 1;
             }
+        }
+    }
+
+    /// Takes in a directory and loads in and processes each vm file within
+    fn process_directory(&mut self, dir_path: &str) {
+        let filenames = read_dir(dir_path).unwrap();
+        
+        // Loop through each file in the directory and list vm files
+        for filename in filenames {
+            let file = filename.unwrap().path().as_os_str().to_str().unwrap().to_owned();
+
+            if file.contains(".vm") {
+                self.process_file(file.as_str());
+            }
+
+        }
+
+    }
+
+    fn process_project(&mut self) {
+        // Check if this is a file or directory
+        let md = metadata(self.vm_path.as_str()).unwrap();
+        if md.is_dir() {
+            self.file_type = FileType::Directory;
+        }
+        else {
+            self.file_type = FileType::File;
+        }
+
+        // Process for file
+        match self.file_type {
+            FileType::File => {
+                self.process_file(self.vm_path.clone().as_str())
+            },
+            FileType::Directory => {
+                self.write_init();
+                self.process_directory(self.vm_path.clone().as_str())
+            },
+            _ => panic!("Input path ({}) is not a valid path", self.vm_path)
         }
 
     }
